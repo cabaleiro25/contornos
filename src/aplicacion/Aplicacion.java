8861291004595b35ffe4748dcbe9229093f2579e
jcabaleiromendez <jcabaleiromendez@eniac13>
2014-03-13 09:49:22 +0100

    segundo comimit(primero de jesus)

---
+++ src/aplicacion/Aplicacion.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion;
+
+/**
+ *
+ * @author jcabaleiromendez
+ */
+public class Aplicacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
